29222a0ee51851524c80116f11bc841540c55ca3
MILANDOU ERVIN <3260058@ppti-14-508-12.ufr-info-p6.jussieu.fr>
2016-01-04 09:55:48 +0100

    [ADD] Création des interfaces modèle

---
+++ PetriSar/src/model/ArcIF.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author 3260058
+ */
+public interface ArcIF {
+    public int getPoids();
+    public void setPoids(int i);
+    
+    public PlaceIF getPlace();
+    public void setPlace(PlaceIF pi);
+    
+}
