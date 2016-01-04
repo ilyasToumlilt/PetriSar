c804dee88933f22d14400123a62485b11a793923
Redha Gouicem <redha.gouicem@gmail.com>
2016-01-04 11:20:10 +0100

    "Firing interface"

---
+++ PetriSar/src/petrisar/IFiring.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package petrisar;
+
+/**
+ *
+ * @author 3100161
+ */
+public interface IFiring extends Runnable {
+    
+}
