b390cb0c49632be24ec0f44aec5593586e0cdba0
Redha Gouicem <redha.gouicem@gmail.com>
2016-01-04 10:44:43 +0100

    "IState interface"

---
+++ PetriSar/src/petrisar/IState.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package petrisar;
+
+/**
+ *
+ * @author Maxime Bittan, Redha Gouicem, Ilyas Toumlilt
+ */
+public interface IState {
+    public int getValue();
+}
