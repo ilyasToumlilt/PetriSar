c257936e2b142275075cec2a677663d705a8a997
Redha Gouicem <redha.gouicem@gmail.com>
2016-01-04 10:35:50 +0100

    "IQueue interface"

---
+++ PetriSar/src/petrisar/IQueue.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package petrisar;
+
+/**
+ *
+ * @author Maxime Bittan, Redha Gouicem, Ilyas Toumlilt
+ */
+public interface IQueue {
+    public IState pop();
+    public void push(IState state);
+    public boolean isEmpty();
+}
